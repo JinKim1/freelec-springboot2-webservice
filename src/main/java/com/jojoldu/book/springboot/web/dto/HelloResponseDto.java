c2abdee6ad43f15ecd3960af5712a70d7a0cf193
jimkim1 <oceanbelow@naver.com>
2020-08-05 21:39:58 +0900

    UI구성 및, 게시판 기본 기능 완성

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -5,14 +5,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 
 public class HelloResponseDto {
     private final String name;
     private final int amount;
 
-    public HelloResponseDto(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
-    }
 }
